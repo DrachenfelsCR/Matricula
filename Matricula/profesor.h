643bad99c14d2716b92f2ac0c6e59a5faffdf279
alberto esquivel gomez <albert.543@hotmail.com>
2020-04-16 21:27:48 -0600

    fuck

--- Matricula/profesor.h
+++ Matricula/profesor.h
@@ -9,7 +9,7 @@ protected:
 	int telefono;
 	string escuela;
 	string gradoAcademico;
-//  lista grupos
+	lista<grupo>* group;
 public:
 	profesor(string, string, string, string);
 	void setTelefono(int);
